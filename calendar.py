f45d8daebd3988973aa83a9e4f2e7c744c128949
PePeLLee <61408245+PePeLLee@users.noreply.github.com>
2024-06-15 20:20:53 +0200

    New API in use

--- calendar.py
+++ calendar.py
@@ -18,14 +18,13 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entities):
     """Konfiguracja za pomcą przepływu konfiguracji."""
-    
+
     """This one is in use"""
     async_add_entities([RCECalendar()])
 
 
 class RCECalendar(CalendarEntity):
     """Representation of a Sensor."""
-    
 
     def __init__(self) -> None:
         _LOGGER.info("RCE calendar")
@@ -37,7 +36,7 @@ class RCECalendar(CalendarEntity):
         self.last_network_pull = datetime(
             year=2000, month=1, day=1, tzinfo=timezone.utc
         )
-        self._attr_unique_id = "rce_calendar" 
+        self._attr_unique_id = "rce_calendar"
         self._attr_name = "RCE calendar"
 
 
@@ -74,11 +73,9 @@ class RCECalendar(CalendarEntity):
     def fetch_cloud_data(self):
         """fetch today data"""
         now = datetime.now(ZoneInfo(self.hass.config.time_zone))
+        url = f"https://api.raporty.pse.pl/api/rce-pln?$filter=doba eq '{now.strftime('%Y-%m-%d')}'"
         try:
-            self.cloud_response = requests.get(
-                f"https://www.pse.pl/getcsv/-/export/csv/PL_CENY_RYN_EN/data/{now.strftime('%Y%m%d')}",
-                timeout=10,
-            )
+            self.cloud_response = requests.get(url, timeout=10)
             self.cloud_response.encoding = 'ISO-8859-2'
 
         except ReadTimeout:
@@ -87,18 +84,16 @@ class RCECalendar(CalendarEntity):
     def fetch_cloud_data_1(self):
         """fetch tomorrow data"""
         now = datetime.now(ZoneInfo(self.hass.config.time_zone)) + timedelta(days=1)
+        url = f"https://api.raporty.pse.pl/api/rce-pln?$filter=doba eq '{now.strftime('%Y-%m-%d')}'"
         try:
-            self.cloud_response = requests.get(
-                f"https://www.pse.pl/getcsv/-/export/csv/PL_CENY_RYN_EN/data/{now.strftime('%Y%m%d')}",
-                timeout=10,
-            )
+            self.cloud_response = requests.get(url, timeout=10)
             self.cloud_response.encoding = 'ISO-8859-2'
         except requests.exceptions.ReadTimeout:
             self.cloud_response = ""
 
     def csv_to_events(self, csv_reader: csv, day: datetime):
         """Transform csv to events"""
-        
+
         for row in csv_reader:
             if not row[1].isnumeric():
                 continue
@@ -111,6 +106,39 @@ class RCECalendar(CalendarEntity):
                 )
             )
             event_start = int(row[1])
+
+
+    def json_to_events(self, json, day: datetime):
+        curr_price = None
+        start_time = None
+        end_time = None
+        for i in json['value']:
+            times =  i['udtczas_oreb'].split("-")
+            ts = datetime.strptime(times[0].strip(),"%H:%M")
+            ts = day.replace(hour=ts.hour, minute=ts.minute, second=0)
+            if times[1].strip()=="24:00":
+                te = day.replace(hour=0, minute=0, second=0) + timedelta(days=1)
+            else:
+                te = datetime.strptime(times[1].strip(),"%H:%M")
+                te = day.replace(hour=te.hour, minute=te.minute, second=0)
+            if(i['rce_pln']!=curr_price):
+                if(curr_price):
+                    #if ts==end_time:
+                    #    end_time = end_time - timedelta(seconds=1)
+                    self.ev.append(
+                        CalendarEvent( start_time, end_time,curr_price,
+                            description="https://www.pse.pl/dane-systemowe/funkcjonowanie-rb/raporty-dobowe-z-funkcjonowania-rb/podstawowe-wskazniki-cenowe-i-kosztowe/rynkowa-cena-energii-elektrycznej-rce",
+                        )
+                    )
+                curr_price = i['rce_pln']
+                start_time = ts
+            end_time = te
+        self.ev.append(
+            CalendarEvent( start_time, end_time,curr_price,
+                        description="https://www.pse.pl/dane-systemowe/funkcjonowanie-rb/raporty-dobowe-z-funkcjonowania-rb/podstawowe-wskazniki-cenowe-i-kosztowe/rynkowa-cena-energii-elektrycznej-rce",
+                    )
+        )
+
 
     async def async_update(self):
         """Retrieve latest state."""
@@ -125,9 +153,9 @@ class RCECalendar(CalendarEntity):
             return False
         self.ev.clear()
 
-        csv_output = csv.reader(self.cloud_response.text.splitlines(), delimiter=";")
         now = now.replace(minute=0).replace(second=0)
-        self.csv_to_events(csv_output, now)
+        self.json_to_events(self.cloud_response.json(), now)
+        #CalendarEvent( now, now, now, description=self.cloud_response)
 
         self.cloud_response = None
         await self.hass.async_add_executor_job(self.fetch_cloud_data_1)
@@ -135,6 +163,7 @@ class RCECalendar(CalendarEntity):
         if self.cloud_response is None or self.cloud_response.status_code != 200:
             return False
 
-        csv_output = csv.reader(self.cloud_response.text.splitlines(), delimiter=";")
         now = now.replace(minute=0).replace(second=0) + timedelta(days=1)
-        self.csv_to_events(csv_output, now)
+        self.json_to_events(self.cloud_response.json(), now)
+        #CalendarEvent( now, now, now, description=self.cloud_response)
+
