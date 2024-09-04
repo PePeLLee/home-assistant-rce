064799c3e89e8bc21359e43ac2f71ffe2832d375
PePeLLee <61408245+PePeLLee@users.noreply.github.com>
2024-09-04 13:15:24 +0200

    Update __init__.py
    Fix init

--- __init__.py
+++ __init__.py
@@ -1,24 +1,22 @@
 """The example sensor integration."""
 
 from homeassistant.core import HomeAssistant
-
+from homeassistant.config_entries import SOURCE_SYSTEM, ConfigEntry
 
 DOMAIN = "rce"
-
-
 async def async_setup(hass: HomeAssistant, config):
     """Wstepna konfiguracja domeny, jeśli to konieczne."""
     return True
 
-
-async def async_setup_entry(hass: HomeAssistant, config_entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Konfigurowanie integracji na podstawie wpisu konfiguracyjnego."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "calendar")
-    )
+    # hass.async_create_task(
+    #     hass.config_entries.async_forward_entry_setup(config_entry, "calendar")
+    # )
+    await hass.config_entries.async_forward_entry_setups(entry, [ "calendar" ] )
     return True
 
-
-async def async_unload_entry(hass: HomeAssistant, config_entry):
+async def async_unload_entry(hass: HomeAssistant,  entry: ConfigEntry):
     """Usunięcie integracji - skasowanie wpis konfiguracyjnego ."""
-    return True
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, [ "calendar" ] )
+    return unload_ok
